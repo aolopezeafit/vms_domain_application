99b6983583cea0e8fb05c81ee47d74c415f16c9f
aolopezeafit <aolopezh@eafit.edu.co>
2023-12-28 07:56:04 -0500

    Generation serena

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || '127.0.0.1',
     PORT: process.env.PORT || 8081,
-    VERSION: '1.23.09.29.06'
+    VERSION: '1.23.12.28.07'
 }
