a90b3c265b6a2f885127a3b661860197dea2ac64
aolopezeafit <aolopezh@eafit.edu.co>
2024-02-09 08:16:10 -0500

    Feature model generation

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || '127.0.0.1',
     PORT: process.env.PORT || 8081,
-    VERSION: '1.24.01.26.06'
+    VERSION: '1.24.02.09.08'
 }
